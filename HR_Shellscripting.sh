8ab4c13c1e677190549a7065d282d03defa47648
SaiRupa Paladugu <30324744+sai1408@users.noreply.github.com>
2023-12-17 22:00:24 -0500

    Update HR_Shellscripting.sh

--- HR_Shellscripting.sh
+++ HR_Shellscripting.sh
@@ -36,4 +36,52 @@ read b
 while read line;do
 echo $line | cut -c3
 done
+#8.Display the  2nd and 7th  character from each line of text.
+while read line;do
+echo $line | cut -c 2,7
+done
+#9 .Display the  2nd and 7th  character from each line of text.
+while read line;do
+echo $line | cut -c 2-7
+done
+#10 Display 1st to 4th character from each line of text
+while read line;do
+echo $line | cut -c 1-4
+done
+#11Given a tab delimited file with several columns (tsv format) print the first three fields.
+while read line;do
+# echo $line | cut -f -3
+# done
+echo "$line" | cut -d$'\t' -f 1-3
+done
+#12.Print the characters from thirteenth position to the end.
+while read line;do
+# echo $line | cut -f -3
+# done
+echo "$line" | cut -c 13-
+done
+#13.Given a sentence, identify and display its fourth word. Assume that the space (' ') is the only delimiter between words.
+while read line;do
+# echo $line | cut -f -3
+# done
+echo "$line" | cut -d " " -f 4
+done
+#14.Given a sentence, identify and display its first three words. Assume that the space (' ') is the only delimiter between words.
+while read line;do
+echo "$line" | cut -d " " -f 1-3
+done
+#15.Given a tab delimited file with several columns (tsv format) print the fields from second fields to last field.
+while read line;do
+# echo $line | cut -f -3
+# done
+echo "$line" | cut -d$'\t' -f 2-
+done
+#16.In this challenge, we practice using the head command to display the first  lines of a text file.
+
+Display the first  lines of an input file.
+head -n 20
+#17.In this challenge, we practice using the head command to display the first  characters of a text file.
+head -c 20
+
+
 
